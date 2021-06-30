a43e1cba3b0b9329b0fd3e0a0c5fe4aa763fe63b
Joshua Sierles <joshua@chatterbug.com>
2021-06-30 10:05:28 +0200

    use cookies instead of the session to track regional preferences

--- lib/fly-rails/capture_readonly.rb
+++ lib/fly-rails/capture_readonly.rb
@@ -26,31 +26,26 @@ module Fly
         "<html>Replaying request in #{primary_region}</html>"
       end
 
-      def respond_with_redirect_to_primary_region
-        [409, {"fly-replay" => "region=#{primary_region}"}, [response_body]]
-      end
-
-      def inject_regional_preference_in_rack_session(env)
-        # Trick to force the session to load, in case it's not been used yet in the application
-        env["rack.session"].delete(:force_session_load)
-        env["rack.session"]["fly_redirect_threshold"] = 2.seconds.from_now.to_i
+      def respond_with_redirect_to_primary_region(env)
+        res = Rack::Response.new(response_body, 409, {"fly-replay" => "region=#{primary_region}"})
+        res.set_cookie("fly-redirect-threshold", 2.seconds.from_now.to_i)
+        res.finish
       end
 
       def region_preferred?(env)
-        threshold = env["rack.session"]["fly_redirect_threshold"]
-        return true if threshold && (threshold - Time.now.to_i) > 0
-        env["rack.session"]["fly_redirect_threshold"] = nil
+        request = Rack::Request.new(env)
+        threshold = request.cookies["fly-redirect-threshold"]
+        threshold && (threshold.to_i - Time.now.to_i) > 0
       end
 
       def call(env)
-        return respond_with_redirect_to_primary_region if region_preferred?(env)
+        return respond_with_redirect_to_primary_region(env) if region_preferred?(env)
 
         begin
           response = @app.call(env)
         rescue ActiveRecord::StatementInvalid => e
           if e.cause.is_a?(PG::ReadOnlySqlTransaction)
-            inject_regional_preference_in_rack_session(env)
-            return respond_with_redirect_to_primary_region
+            return respond_with_redirect_to_primary_region(env)
           else
             raise e
           end

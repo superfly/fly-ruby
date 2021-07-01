ffc3e6c20a67d8259c618baacb952a035c840fd1
Joshua Sierles <joshua@chatterbug.com>
2021-07-01 20:58:44 +0200

    make library configurable

--- lib/fly-rails.rb
+++ lib/fly-rails.rb
@@ -1,7 +1,28 @@
 require_relative "fly-rails/rails"
 require_relative "fly-rails/configuration"
+require "forwardable"
 
-def init(&block)
-  config = Configuration.new
-  yield(config) if block
+module Fly
+  class << self
+    extend Forwardable
+
+    def instance
+      @instance ||= Instance.new
+    end
+
+    def_delegators :instance, :configuration, :configuration=
+  end
+
+  class Instance
+    attr_accessor :configuration
+
+    def initialize
+      self.configuration = Fly::Configuration.new
+    end
+
+    def configure
+      yield(configuration) if block_given?
+      self.configuration = Fly::Configuration.new(configuration)
+    end
+  end
 end
--- lib/fly-rails/regional_database.rb
+++ lib/fly-rails/regional_database.rb
@@ -1,45 +1,61 @@
-require_relative "environment"
-
 module Fly
   module Rails
     class RegionalDatabase
-      include Environment
-
       def initialize(app)
         @app = app
-        prefer_regional_database
+        prefer_regional_database! unless in_primary_region?
       end
 
-      def prefer_regional_database
-        return if primary_region == current_region
-        ENV["DATABASE_URL"] = regional_database_url
+      # Overwrite the primary database URL with that of the regional replica
+      def prefer_regional_database!
+        ENV[Fly.configuration.database_url_env_var] = regional_database_url
+      end
+
+      def in_primary_region?
+        Fly.configuration.primary_region == Fly.configuration.current_region
       end
 
       def regional_database_url
         uri = URI.parse(database_url)
-        uri.hostname = "#{current_region}.#{uri.hostname}"
+        uri.hostname = "#{Fly.configuration.current_region}.#{uri.hostname}"
         uri.port = 5433
         uri.to_s
       end
 
       def response_body
-        "<html>Replaying request in #{primary_region}</html>"
+        "<html>Replaying request in #{Fly.configuration.primary_region}</html>"
       end
 
-      def respond_with_redirect_to_primary_region
-        res = Rack::Response.new(response_body, 409, {"fly-replay" => "region=#{primary_region}"})
-        res.finish
+      # Override the configured database URL with that of the regional replica
+      def replay_in_primary_region!
+        [409, {"fly-replay" => "region=#{Fly.configuration.primary_region}"}, response_body]
       end
 
-      def region_preferred?(request)
-        threshold = request.cookies["fly-redirect-threshold"]
+      # Check whether this request satisfies any of the following conditions for replaying in the primary region:
+      #
+      # 1. It arrived before the threshold defined by the last write request. This threshold
+      #    helps avoid the same client from missing its own write due to replication lag,
+      #    like when a user updates a todo list via XHR
+      #
+      # 2. Its HTTP method matches those configured for automatic replay (post/patch/put/delete by default).
+      #    This approach should avoid potentially slow code execution - before_actions or other controller code -
+      #    happening before a request reaches a database write.
+      #
+      def primary_region_preferred?(request)
+        return true if Fly.configuration.replay_http_methods.include?(request.request_method)
+
+        threshold = request.cookies[Fly.configuration.replay_threshold_cookie]
         threshold && (threshold.to_i - Time.now.to_i) > 0
+      end
+
+      def replayed?(request)
+        request.get_header("HTTP_#{Fly.configuration.fly_dispatch_header.upcase.gsub("-", "_")}")&.scan(/t/)&.count == 2
       end
 
       def call(env)
         request = Rack::Request.new(env)
 
-        return respond_with_redirect_to_primary_region if region_preferred?(request)
+        return replay_in_primary_region! if primary_region_preferred?(request)
 
         begin
           status, headers, body = @app.call(env)
@@ -53,8 +69,11 @@ module Fly
         # Request was replayed, so set a regional preference for the next 5 seconds
         response = Rack::Response.new(body, status, headers)
 
-        if request.get_header("HTTP_FLY_DISPATCH_START")&.scan(/t/)&.count == 2
-          response.set_cookie("fly-redirect-threshold", 5.seconds.from_now.to_i)
+        if replayed?(request)
+          response.set_cookie(
+            Fly.configuration.replay_threshold_cookie,
+            Time.now.to_i + Fly.configuration.replay_threshold_in_seconds
+          )
         end
 
         response.finish

02e5ca5cedd5a09f09096fe46def4d45f0e1d16f
Joshua Sierles <joshua@chatterbug.com>
2021-07-14 21:24:35 +0200

    set Fly-specific headers to assist with debugging

--- lib/fly-ruby/railtie.rb
+++ lib/fly-ruby/railtie.rb
@@ -1,16 +1,25 @@
 class Fly::Railtie < Rails::Railtie
-  initializer("fly.regional_database") do |app|
+  initializer('fly.regional_database') do |app|
     if Fly.configuration.eligible_for_activation?
       # Run the middleware high in the stack, but after static file delivery
       app.config.middleware.insert_after ActionDispatch::Executor, Fly::RegionalDatabase
 
-      # If we already have a database connection when this initializer runs,
-      # we should reconnect to the region-local database. This may need some additional
-      # hooks for forking servers to work correctly.
-      if defined(ActiveRecord) && ActiveRecord::Base.connected?
-        config = Rails.application.config.database_configuration[Rails.env]
-        ActiveRecord::Base.connect_to(config)
+      ActiveSupport::Reloader.to_prepare do
+        # If we already have a database connection when this initializer runs,
+        # we should reconnect to the region-local database. This may need some additional
+        # hooks for forking servers to work correctly.
+        if defined(ActiveRecord) && ActiveRecord::Base.connected?
+          config = Rails.application.config.database_configuration[Rails.env]
+          ActiveRecord::Base.connect_to(config)
+        end
+
+        # Set useful headers for debugging
+        ApplicationController.send(:after_action) do
+          response.headers['Fly-Region'] = ENV['FLY_REGION']
+          response.headers['Fly-Database-Host'] = ENV[Fly.configuration.database_host_env_var]
+        end
       end
+
     elsif Fly.configuration.web?
       puts "Warning: DATABASE_URL, PRIMARY_REGION and FLY_REGION must be set to activate the fly-ruby middleware. Middleware not loaded."
     end
